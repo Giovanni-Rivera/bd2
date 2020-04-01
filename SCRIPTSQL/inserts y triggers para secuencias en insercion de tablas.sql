24c8c00db1b5c68ede183d040738e3cf0539510b
Giovanni-Rivera <wgriveral@hotmail.com>
2020-04-01 05:47:49 -0600

    agregando secuencia para la tabla tabla_configuraion

--- SCRIPTSQL/inserts y triggers para secuencias en insercion de tablas.sql
+++ SCRIPTSQL/inserts y triggers para secuencias en insercion de tablas.sql
@@ -9,3 +9,9 @@ VAMOS A COLOCAR SEQ_TBL_NOMBRE_TABLA*/
 TRIG_(BF,AF,INST)_NOMBRE_TABLA*/
 
 /***********************************************************************************************/
+
+/*tabla configuracion*/
+/*creando la secuencia correspondiente*/
+CREATE SEQUENCE SEQ_TBL_TBLCONFIG
+START WITH 1
+INCREMENT BY 1;
