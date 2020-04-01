988b1a4cfe514c815c1839a9a061c90498f7d830
Giovanni-Rivera <wgriveral@hotmail.com>
2020-04-01 05:37:04 -0600

    agregando secuencia para la tabla tabla_configuraicon

--- SCRIPTSQL/script final.sql
+++ SCRIPTSQL/script final.sql
@@ -658,3 +658,11 @@ VAMOS A COLOCAR SEQ_TBL_NOMBRE_TABLA*/
 
 /*PARA EL NOMBRE DE TRIGGERS
 TRIG_(BF,AF,INST)_NOMBRE_TABLA*/
+
+
+/***********************************************************************************************/
+/*tabla configuracion*/
+/*creando la secuencia correspondiente*/
+CREATE SEQUENCE SEQ_TBL_TBLCONFIG
+START WITH 1
+INCREMENT BY 1;
